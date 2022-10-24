aba97a869ad083591855b5692cb66fb51f8b13d5
sabrababra <sabrinayesminprome@gmail.com>
2022-10-24 23:30:17 +0600

    router set up complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import {router} from './Routes/Route'
 
 function App() {
   return (
     <div className="App">
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Routes/Route.js
@@ -0,0 +1,36 @@
+import Main from "../layout/Main";
+import Blogs from "../pages/Blogs/Blogs";
+import Course from "../pages/Course/Course";
+import FAQ from "../pages/FAQ/FAQ";
+import Home from "../pages/Home/Home";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+export const router=createBrowserRouter([
+    {
+        path:'/',
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'/home',
+                element:<Home></Home>
+            },
+            {
+                path:'/course',
+                element:<Course></Course>
+            },
+            {
+                path:'/blogs',
+                element:<Blogs></Blogs>
+            },
+            {
+                path:'/faq',
+                element:<FAQ></FAQ>
+            },
+        ]
+    }    
+])
